cf33640d1b136fd0bfce768e575ba04693402735
Kapoor27 <122999145+Kapoor27@users.noreply.github.com>
2023-02-24 14:29:12 +0530

    total count charac

--- src/duplicate_string_character/totalnumberofcountstring.java
+++ src/duplicate_string_character/totalnumberofcountstring.java
@@ -3,7 +3,18 @@ package duplicate_string_character;
 public class totalnumberofcountstring {
 
         public static void main(String[] args) {
+      String s = "my name is nitin kapoor";
+      int count = 0;
 
+      System.out.println("lenght of thr given string " +s.length());  //for count space
+
+      for(int i=0; i<s.length(); i++){
+
+          if(s.charAt(i)!= ' '){
+              count++;
+          }
+      }
+        System.out.println("count the given String " +count); //for count without space
 
         }
     }
